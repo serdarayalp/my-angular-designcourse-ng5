9d3b354e4579f4a235c0251c2cdd23bad8235045
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-30 09:01:02 +0100

    kleine Änderungen

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  itemCount: number = 4;
+  btnText: string = 'Add an Item';
+  goalText: string = '';
+  goals = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+}
